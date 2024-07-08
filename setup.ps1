c6ec47bf39d29dd4554c658c1087f23be7d971f5
ornfelt <jonasornfelt@outlook.com>
2024-07-08 15:27:24 +0200

    setup script

---
+++ setup.ps1
@@ -0,0 +1,8 @@
+# Fix dll path for stormlib
+# Fix client data
+# Blp code should be set already...
+# However, the mpq path needs to be set in it:
+# lib/server/pipeline/index.js
+
+# node_modules/blizzardry/lib/mpq/storm-lib.js
+# node_modules/blizzardry/lib/blp/blp-lib.js
